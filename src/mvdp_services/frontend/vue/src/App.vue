0dff342e4dd62005e74eefe7cc165525e5466cb4
Volodymyr Drobitko <volodymyr.drobitko@ivv-dd.fraunhofer.de>
2023-01-25 11:56:42 +0100

    UploadView

--- src/mvdp_services/frontend/vue/src/App.vue
+++ src/mvdp_services/frontend/vue/src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home!!!</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/upload">Upload</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/mvdp_services/frontend/vue/src/views/UploadView.vue
@@ -0,0 +1,5 @@
+<template>
+    <form>
+        <input type="file"/>
+    </form>
+</template>
