73b6aa6dbbbffdd930d283cff862d99dbd3028f9
Volodymyr Drobitko <volodymyr.drobitko@ivv-dd.fraunhofer.de>
2023-01-25 13:02:29 +0100

    upload image example

--- src/mvdp_services/frontend/vue/src/views/UploadView.vue
+++ src/mvdp_services/frontend/vue/src/views/UploadView.vue
@@ -1,5 +1,41 @@
 <template>
-    <form>
-        <input type="file"/>
-    </form>
+    <div style="border-style:solid">
+      <input type="file" ref="file" @change="readFile()" />
+      <div v-if="Image">
+        <img :src="preview" />
+      </div>
+    </div>
 </template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            example: null,
+            Image: false,
+            preview: null
+        }
+    },
+    methods: {
+    readFile() {
+      this.example = this.$refs.file.files[0];
+      if (
+        this.example.name.includes(".png") ||
+        this.example.name.includes(".jpg")
+      ) {
+        this.Image = true;
+        this.preview = URL.createObjectURL(this.example);
+      } else {
+        this.Image = false;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .imgBox {
+        height: 100px;
+        width: 100px;
+    }
+</style>
