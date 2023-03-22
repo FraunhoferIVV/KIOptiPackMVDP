4a00b082e37c518fd9925dc41cc6034ea1efba7f
Volodymyr Drobitko <volodymyr.drobitko@ivv-dd.fraunhofer.de>
2023-03-22 16:56:27 +0100

    some code clean up

--- src/mvdp_services/data_provider/data_provider_service.py
+++ src/mvdp_services/data_provider/data_provider_service.py
@@ -8,9 +8,8 @@ from typing import List
 
 import pandas as pd
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastiot.core import FastIoTService
-from fastiot.msg.thing import Thing
 from fastiot.db.mongodb_helper_fn import get_mongodb_client_from_env
 from fastiot.env import env_mongodb
 from fastiot.msg.custom_db_data_type_conversion import from_mongo_data
@@ -26,14 +25,9 @@ class DataProviderService(FastIoTService):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # TODO: connect pipe input with a database, output with EDC
         self.mongo_db_client = get_mongodb_client_from_env()
         database = self.mongo_db_client.get_database(env_mongodb.name)
-        """
-        self._mongodb_handler = MongoDBHandler()
-        database = self._mongodb_handler.get_database(env_mongodb.name)
-        """
-        #  potentially asset from the database (db queries: Asset_x -> [column1, column2, ...])
+        # potentially load asset from the database (db queries: Asset_x -> [column1, column2, ...])
         service_config = read_config(self)
         if not service_config:
             self._logger.error('Please set the config as shown in the documentation! Aborting service!')
@@ -58,7 +52,7 @@ class DataProviderService(FastIoTService):
             allow_methods=['GET'],
             allow_headers=["*"],
         )
-        self.app.get("/assets/Asset_{asset_id}")(self._handle_get)
+        self.app.get("/assets/{asset_name}")(self._handle_get)
         # no mounting apps at first
         """
         try:
@@ -77,27 +71,6 @@ class DataProviderService(FastIoTService):
             if attr.startswith('Asset_'):
                 self.assets[attr] = content.split(", ")
 
-    """
-    def _create_index(self, mongo_indices):
-        for index in mongo_indices:
-            if "," in index:  # Build compound index
-                indices = index.split(",")
-                indices = [i.strip() for i in indices]
-                compound_index = list(zip(indices,
-                                          map(lambda index_name:
-                                              pymongo.ASCENDING if index_name != '_timestamp' else pymongo.DESCENDING,
-                                              indices)))
-                # the later the _timestamp in mongo_data - the more time relevant query results
-                self._logger.debug(compound_index)
-                self._mongodb_handler.create_index(collection=self._mongo_object_db_col,
-                                                   index_name="compound_index",
-                                                   index=compound_index)
-            else:
-                self._mongodb_handler.create_index(collection=self._mongo_object_db_col,
-                                                   index=[(index, pymongo.ASCENDING)],
-                                                   index_name=f"{index}_ascending")
-    """
-
     async def _start(self):
         """ Methods to start once the module is initialized """
         await self.server.up()
@@ -106,24 +79,27 @@ class DataProviderService(FastIoTService):
         """ Methods to call on module shutdown """
         await self.server.down()
 
-    def _handle_get(self, asset_id: str):
-        asset_name = 'Asset_' + asset_id
+    def _handle_get(self, asset_name: str):
+        if asset_name not in list(self.assets.keys()):
+            raise HTTPException(status_code=500, detail='Asset not configured!')
         query = {"name": {"$in": self.assets[asset_name]}}
-        result = self.mongodb_col.find(query)
-        # create list of things
+        result = self.mongodb_col.find(query)  # create list of things
         things = list(map(from_mongo_data, result))
-        rows = self._things_to_rows(things, asset_name)
+        rows = self._things_to_rows(things)
         data_frame = pd.DataFrame.from_records(rows)
         self._logger.debug('\n' + str(data_frame))
         return data_frame.to_json()
 
-    def _things_to_rows(self, things: List, asset_name):
-        # (timestamp, measurement_id) identifies row
-        # sort by (timestamp, measurement_id, name) ascending => row by row, columns sorted
-        things.sort(key=lambda thing: (thing['timestamp'], thing['measurement_id'], thing['name']))
-        columns = sorted(self.assets[asset_name])
+    @ staticmethod
+    def _things_to_rows(things: List):
+        # empty list (exception state)
+        if not things:
+            return []
+        # (timestamp, measurement_id) identifies row; make things order row by row
+        things.sort(key=lambda th: (th['timestamp'], th['measurement_id']))
         rows = []
-        # possibly columns from different tables
+        # possibly columns from different tables (otherwise only check index instead of key)
+        # init helper variables for the first list element
         key_now = (things[0]['timestamp'], things[0]['measurement_id'])
         row_now = {'Timestamp': things[0]['timestamp'],
                    'Material_ID': things[0]['measurement_id']}
@@ -132,13 +108,11 @@ class DataProviderService(FastIoTService):
             if thing_key != key_now:  # this thing is from new row
                 rows.append(row_now)  # push previous row
                 row_now = {'Timestamp': thing['timestamp'],
-                           'Material_ID': thing['measurement_id']}
-                key_now = thing_key
+                           'Material_ID': thing['measurement_id']}  # init current row
+                key_now = thing_key  # set current key_now
             row_now[thing['name']] = str(thing['value']) + ' ' + str(thing['unit'])  # create new table cell
         rows.append(row_now)  # push the last row
         return rows
-
-
 
 
 if __name__ == '__main__':
