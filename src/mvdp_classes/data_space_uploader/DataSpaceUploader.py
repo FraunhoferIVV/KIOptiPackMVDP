3d748dff9682736fa77d54a942306c85c6237403
Volodymyr Drobitko <volodymyr.drobitko@ivv-dd.fraunhofer.de>
2023-03-30 15:35:14 +0200

    sending reduced dataframes in batches

--- src/mvdp_classes/data_space_uploader/DataSpaceUploader.py
+++ src/mvdp_classes/data_space_uploader/DataSpaceUploader.py
@@ -21,22 +21,38 @@ class DataSpaceUploader:
         self.max_post_len = max_post_len
 
     def upload(self, material_id: str, parameters: DataFrame, values: DataFrame):
+        self._upload_dataframe(material_id, parameters)
+        self._upload_dataframe(material_id, values)
+
+    def _upload_dataframe(self, material_id, dataframe: DataFrame):
         post_url = "http://0.0.0.0:5480"
-        # parameters df to message batches
-        # parameters = self._build_dataframe_differnces(parameters)
-        parameters_batches = np.array_split(parameters, len(parameters) / self.max_post_len + 1)
-        # values df to message batches
-        # values = self._build_dataframe_differnces(values)
-        values_batches = np.array_split(values, len(values) / self.max_post_len + 1)
-        # all message batches
-        post_batches = parameters_batches + values_batches  # list of dataframes
+        df_columns = DataSpaceUploader._reduce_dataframe(dataframe)
+        post_batches = []
+        for column in df_columns:
+            split_index = len(column) // self.max_post_len + 1
+            post_batches += np.array_split(column, split_index)
         for batch in post_batches:
             msg = {
                 "material_id": material_id,
                 "content": batch.to_json()
             }
+            self._logger.debug("Message:" + str(msg))
             # rq.post(post_url, msg)
 
+    @staticmethod
+    def _reduce_dataframe(dataframe: DataFrame):
+        columns_dfs = map(DataSpaceUploader._reduce_columns, [dataframe[col] for col in dataframe])
+        return columns_dfs
+
+    @staticmethod
+    def _reduce_columns(column):
+        same_value_indices = []
+        items = list(column.items())
+        for index, row in items[1:]:
+            if row == items[index - 1][1]:
+                same_value_indices.append(index)
+        column = DataFrame(column).drop(same_value_indices)
+        return column
 
     """
     def upload(self, material_id: str, parameters: DataFrame, values: DataFrame):
@@ -64,7 +80,7 @@ class DataSpaceUploader:
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     dsu = DataSpaceUploader("543")
-    dsu.set_max_pos_len(10)
+    dsu.set_max_pos_len(20)
     df = pd.read_excel("/home/drobitko/Downloads/Protokoll_MotiV.xlsx")
-    res = dsu.upload("722", df["Name"], df[["Parameter", "Werte"]])
+    res = dsu.upload("722", df, DataFrame())
     print(res)
