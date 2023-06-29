4f6a58015018803c4d04a144107b195776b27fab
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2023-06-29 15:21:17 +0200

    Adjust observability api description
    s. https://github.com/eclipse-edc/Connector/issues/3245 for details

--- misc/create_management_api_client.sh
+++ misc/create_management_api_client.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Script to generate the EDC Client from EDC’s OpenAPI specification.
+# For generating the OpenAPI specification please refer to https://github.com/eclipse-edc/Connector/blob/main/docs/developer/openapi.md
+
 if [[ ! -d "${PWD}/src" ]]; then
   echo "You must start this script from the project root, so that 'misc/create_management_api_client.sh' is working!"
   exit 1
 fi
 
 docker run --rm -v "${PWD}:/local" \
-         openapitools/openapi-generator-cli generate \
+         openapitools/openapi-generator-cli:latest generate \
            -i /local/misc/management-api.yml -g python  -o /local/src --skip-validate-spec \
            --additional-properties "library=asyncio,packageName=mvdp.edc_management_client,generateSourceCodeOnly=true"
